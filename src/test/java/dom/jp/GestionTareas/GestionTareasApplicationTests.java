5e43af768ae3b13924cae05b92675950f699b304
Jp <juanpdominguez97@gmail.com>
2025-01-28 10:42:24 -0300

    -

---
+++ src/test/java/dom/jp/GestionTareas/GestionTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package dom.jp.GestionTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
