7620333db24e5d9249d7ee83ff437a13554c3a4d
Jp <juanpdominguez97@gmail.com>
2025-01-29 20:37:52 -0300

    agregar tarea listo

--- src/main/java/dom/jp/GestionTareas/controllers/TareaController.java
+++ src/main/java/dom/jp/GestionTareas/controllers/TareaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -26,18 +27,18 @@ public class TareaController {
 
     @GetMapping({"/","#","index"})
     public String incio(Model model) {
-
+        
         Tarea nuevaTarea = new Tarea();
         model.addAttribute("nuevaTarea", nuevaTarea);
 
-        
         return "index";
     }
     
 
     @PostMapping("tarea/crear")
-    public void crearTarea(@RequestBody Tarea unaTarea) {
+    public String crearTarea(@ModelAttribute Tarea unaTarea) {
         repo.guardarTarea(unaTarea);
+        return "redirect:/";
     }
     
 
