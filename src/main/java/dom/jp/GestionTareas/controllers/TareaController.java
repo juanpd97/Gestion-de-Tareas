1241b52d936d7a29153c5c96a32a5d99beb5039d
Jp <juanpdominguez97@gmail.com>
2025-01-29 20:11:12 -0300

    empezando el formulario para crear una tarea

--- src/main/java/dom/jp/GestionTareas/controllers/TareaController.java
+++ src/main/java/dom/jp/GestionTareas/controllers/TareaController.java
@@ -1,6 +1,16 @@
 package dom.jp.GestionTareas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import dom.jp.GestionTareas.models.Tarea;
+import dom.jp.GestionTareas.services.TareaServiceImpl;
+
+
 
 @Controller
 public class TareaController {
@@ -11,7 +21,25 @@ public class TareaController {
 // Actualizar una tarea.
 // Eliminar una tarea.
 
+    @Autowired
+    TareaServiceImpl repo;
 
+    @GetMapping({"/","#","index"})
+    public String incio(Model model) {
+
+        Tarea nuevaTarea = new Tarea();
+        model.addAttribute("nuevaTarea", nuevaTarea);
+
+        
+        return "index";
+    }
+    
+
+    @PostMapping("tarea/crear")
+    public void crearTarea(@RequestBody Tarea unaTarea) {
+        repo.guardarTarea(unaTarea);
+    }
+    
 
 
 
