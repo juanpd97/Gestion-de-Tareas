3010587272c960a655d18ba24ab711c73fd3522b
Jp <juanpdominguez97@gmail.com>
2025-02-04 10:18:58 -0300

    listo

--- src/main/java/dom/jp/GestionTareas/controllers/TareaController.java
+++ src/main/java/dom/jp/GestionTareas/controllers/TareaController.java
@@ -5,13 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+
 import dom.jp.GestionTareas.models.Tarea;
 import dom.jp.GestionTareas.services.TareaServiceImpl;
+import jakarta.validation.Valid;
 
 
 
@@ -40,8 +44,15 @@ public class TareaController {
     }
     
 
-    @PostMapping("tarea/crear")
-    public String crearTarea(@ModelAttribute Tarea unaTarea) {
+    @PostMapping("/tarea/crear")
+    public String crearTarea(@Valid @ModelAttribute("nuevaTarea") Tarea unaTarea, 
+                            BindingResult result, 
+                            Model model) {
+        if (result.hasErrors()) {
+             List<Tarea> listaTareas = repo.buscarTodasLasTareas();
+             model.addAttribute("listaTareas", listaTareas);
+            return "index";
+        }
         repo.guardarTarea(unaTarea);
         return "redirect:/";
     }
@@ -61,10 +72,17 @@ public class TareaController {
         return "editarTarea";
     }
 
- // filepath: /C:/Users/juanp/OneDrive/Documentos/Gestion-de-Tareas/src/main/java/dom/jp/GestionTareas/controllers/TareaController.java
+ 
 @PostMapping("/tarea/editar")
-public String editarTarea(@ModelAttribute Tarea tarea) {
-    System.out.println("-------**********-------- ID de la tarea a editar: " + tarea.getId());
+public String editarTarea(@Valid @ModelAttribute("tarea") Tarea tarea,
+                         BindingResult result, 
+                         Model model) {
+
+    if(result.hasErrors()){
+        model.addAttribute("tarea", tarea);
+        return "editarTarea";
+    }
+
     repo.editarTarea(tarea);
     return "redirect:/";
 }
--- src/main/java/dom/jp/GestionTareas/models/Tarea.java
+++ src/main/java/dom/jp/GestionTareas/models/Tarea.java
@@ -1,4 +1,3 @@
-// filepath: /c:/Users/juanp/OneDrive/Documentos/Gestion-de-Tareas/src/main/java/dom/jp/GestionTareas/models/Tarea.java
 package dom.jp.GestionTareas.models;
 
 import java.time.LocalDate;
@@ -9,6 +8,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Tarea {
@@ -16,13 +19,28 @@ public class Tarea {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotBlank(message = "El título no puede estar vacío")
     private String titulo;
+
+
     private String descripcion;
-    private String estado; //Por hacer, En progreso, Completada.
+    private String estado;
+
+    @NotNull(message = "debe ingresar una fecha de inicio")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate fechaCreacion;
+    
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate fechaVencimiento;
+
+    @AssertTrue(message = "La fecha de vencimiento debe ser posterior a la fecha de creación")
+    public boolean isFechaVencimientoPosterior() {
+        if (fechaCreacion == null || fechaVencimiento == null) {
+            return true; // Validación no se aplica si alguna de las fechas es nula
+        }
+        return fechaVencimiento.isAfter(fechaCreacion);
+    }
 
     // Getters and Setters
     public int getId() {
